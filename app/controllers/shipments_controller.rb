74118ccac1fa94bcf80ab4f6951d65c3254a0749
Khalid Abbas <khalidabbas.dev@gmail.com>
2023-08-14 14:19:24 -0700

    Add controller for shipments

---
+++ app/controllers/shipments_controller.rb
@@ -0,0 +1,40 @@
+class ShipmentsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :current_trucker
+    before_action :shipment, only: :update
+
+    def index
+        @shipments = current_trucker.shipments
+        render @shipments
+    end
+
+    def create
+        shipment = current_trucker.shipments.create!(shipment_params)
+
+        redirect_to shipments_path, notice: 'Shipment Create Success'
+    end
+
+    def update
+        shipment.update(update_params)
+
+        redirect_to shipments_path, notice: 'Shipment Update Success'
+    end
+
+    private
+
+    def current_trucker
+       Trucker.find_by!(email: params[:trucker_email])
+    end
+
+    def shipment_params
+        params.permit(:lat, :long, :product, :description, :delivery_date)
+    end
+
+    def update_params
+        params.permit(:id, :status, :description)
+    end
+
+    def shipment
+        Shipment.find(update_params[:id])
+    end
+end
